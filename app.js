b1301704d1468c8a2cfdb949fb539e3a13f3702e
Am1d1v <dimadiman1adima@gmail.com>
2023-12-11 17:40:14 +0300

    .

--- app.js
+++ app.js
@@ -1,46 +1,67 @@
 
 
 const container = document.querySelector('.container');
-const btn1 = document.querySelector('.btn');
-const val = document.querySelector('.val');
+const createNew = document.querySelector('.btn');
+const inputValue = document.querySelector('.val');
 const output = document.querySelector('.output');
 const baseurl = 'http://localhost:3000/';
+const createButton = createMyElement(container, 'button', 'Load Posts')
 
 // Fetch posts when page is loaded
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', getAllPosts);
+
+
+// Options for fetch request
+createNew.addEventListener('click', addPost);
+
+
+function pageContents(data){
+    console.log(data);
+}
+
+// Create DOM elements with DB data
+function createMyElement(parent, typeOfElement, html){
+    const element = document.createElement(typeOfElement);
+
+    parent.append(element);
+    element.innerHTML = html;
+    
+    return element;
+}
+
+// Add new post 
+function addPost(event){
+        event.preventDefault();
+    
+        const url = baseurl + 'posts';
+        // Post content
+        const body = {
+            title: inputValue.value,
+            author: 'Author1'
+        };
+        // Fetch options
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        };
+    
+        fetch(url, options)
+        .then(res => res.json())
+        .then(data => {
+            pageContents(data);
+        })
+}
+
+// Show all posts
+function getAllPosts(event){
+
     const url = baseurl + 'posts';
     fetch(url)
     .then(res => res.json())
     .then(data => {
         pageContents(data);
     })
-});
-
-// Options for fetch request
-btn1.addEventListener('click',(event) => {
-    const url = baseurl + 'posts';
-    // Post content
-    const body = {
-        title: val.value,
-        author: 'Author1'
-    };
-    // Fetch options
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        }
-    };
-
-    fetch(url, options)
-    .then(res => res.json())
-    .then(data => {
-        pageContents(data);
-    })
-});
-
-
-function pageContents(data){
-    console.log(data);
 }
